8aae55a3bd04a35ada78c57ad3fcf0ad1e561dd4
Kelsey Chan <kelseyc@mit.edu>
2015-10-27 23:34:02 -0400

    Add getSentences function

--- easymailClient.js
+++ easymailClient.js
@@ -9,3 +9,17 @@ $(document).ready(function(){
 	console.log("WHAT");
 	$('#textEditor').wysiwygEditor();
 })*/
+
+// Given a string of text, returns a list of sentences contained in the text.
+function getSentences(text) {
+    var re = /[\.?!]\s+[A-Z](?!\.)(?=[a-z0-9A-Z\s\W]*[\.?!])/g;
+    var myArray;
+    var sentenceList = [];
+    var lastIndex = 0;
+    while ((myArray = re.exec(text)) !== null) {
+        var punct = myArray.index;                                              // index of punctuation
+        sentenceList.push(text.substring(Math.max(0,lastIndex-1), punct+1))     // add sentence to list
+        lastIndex = re.lastIndex;                                               // update last index accounted for
+    }
+    return sentenceList;
+}
