cbd28ae194e4eead1df175f2b8091fafb57eb0d5
Kelsey Chan <kelseyc@mit.edu>
2015-10-28 14:44:29 -0400

    Fixed bug. Last sentence no longer lost.

--- easymailClient.js
+++ easymailClient.js
@@ -15,11 +15,14 @@ function getSentences(text) {
     var re = /[\.?!]\s+[A-Z](?!\.)(?=[a-z0-9A-Z\s\W]*[\.?!])/g;
     var myArray;
     var sentenceList = [];
-    var lastIndex = 0;
+    var lastIndex = -1;
     while ((myArray = re.exec(text)) !== null) {
         var punct = myArray.index;                                              // index of punctuation
         sentenceList.push(text.substring(Math.max(0,lastIndex-1), punct+1))     // add sentence to list
         lastIndex = re.lastIndex;                                               // update last index accounted for
+    }
+    if (re.lastIndex != -1) {
+        sentenceList.push(text.substring(punct+1, text.length));                // add last sentence
     }
     return sentenceList;
 }
