e3b5c37fe0c08c158abe4b07a6b64474f731ef1c
Priscila Cortez <priscila@mit.edu>
2015-11-18 15:04:04 -0500

    advanced to client

--- easymailClient.js
+++ easymailClient.js
@@ -1,3 +1,63 @@
+$(document).ready(function(){
+  var advancedEditor, authorship, cursorManager, _;
+
+  _ = Quill.require('lodash');
+
+  advancedEditor = new Quill('.advanced-wrapper .editor-container', {
+    modules: {
+      'authorship': {
+        authorId: 'advanced',
+        enabled: true
+      },
+      'toolbar': {
+        container: '.advanced-wrapper .toolbar-container'
+      },
+      'link-tooltip': true,
+      'image-tooltip': true,
+    },
+    styles: false,
+    theme: 'snow'
+  });
+
+  $('.editor-container').css('line-height', $('.linespacing_percentage').val()*100+"%");
+
+  $('.linespacing_percentage').on("input", function(){
+    console.log("HIII");
+    $('.editor-container').css('line-height', $(this).val()*100+'%');
+  });
+
+  $('.quetion_btn').on("input", function(){
+    $('.question').color("black");
+  });
+
+  $('#settings_header').click(function(){
+    $('#settings_panel').hide("slide", {direction: "right"}, 500, function(){
+      $('.settings_button').show();
+    });
+  });
+
+  $('.settings_button').click(function(){
+    $(this).hide();
+    $('#settings_panel').show("slide", {direction: "right"}, 500);
+  });
+
+    
+  $('#reformat_button').click(function() {
+      var rawText = advancedEditor.getHTML();
+      console.log(rawText);
+      if(rawText.indexOf('class="question"') == -1)
+      {
+        var sentenceList = getSentences(rawText);
+        var sentenceHTML = getSentenceHTML(sentenceList);
+        advancedEditor.setHTML(sentenceHTML);
+      }        
+  });
+    
+  $('#clear').click(function() {
+    advancedEditor.setHTML("");
+  })
+})
+
 // Given a string of text, returns a list of sentences contained in the text.
 function getSentences(text) {
     var re = /[\.?!]\s+[A-Z](?!\.)(?=[a-z0-9A-Z\s\W]*[\.?!])/g;
