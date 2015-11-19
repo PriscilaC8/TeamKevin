31922f7739c532e3e85e6a342448de42429ab44a
Alex Noakes <asnoakes@mit.edu>
2015-11-18 22:11:42 -0500

    Easymail now highlights hard coded set of words, there can be more than one highlighted word per sentence, but it will not (currently) highlight the same word twice in a sentence.

--- easymailClient.js
+++ easymailClient.js
@@ -47,10 +47,9 @@ $(document).ready(function(){
   });
 
   $('#ql-editor-1').keyup(function(){
-    console.log(advancedEditor.getLength());
     if(advancedEditor.getLength() == 1)  //the quill editor doesn't go down to zero characters
     {
-      console.log("Hi");
+      // console.log("Hi");
       $('#reformat_button').removeAttr('disabled');
     }
   });
@@ -58,7 +57,7 @@ $(document).ready(function(){
   $('#reformat_button').click(function() {
       $('#reformat_button').attr('disabled','true');
       var rawText = advancedEditor.getHTML();
-      console.log(rawText);
+      // console.log(rawText);
       if(rawText.indexOf('class="question"') == -1)
       {
         var sentenceList = getSentences(rawText);
@@ -110,15 +109,56 @@ function isQuestion(sentence) {
     return sentence.match(re) != null;
 }
 
+function findWords(sentence) {
+    var words = ["Could","Would","Should","Can you","conclusion"];
+    var indexes = [];
+    for(i = 0; i<5; i++){
+        if(sentence.indexOf(words[i]) != -1)
+        {
+            indexes.push([sentence.indexOf(words[i]),words[i].length]);
+            console.log("found one");
+        }
+    }
+    return indexes
+}
+
+function sentenceReconstructor(sentence, startIndex, indexes){ //this is the probably overly complicated way to highlight more than one word per sentence.
+    var newHtml = '';
+    if(indexes.length == 1)
+    {
+        newHtml += sentence.substring(startIndex, indexes[0][0]);
+        newHtml += '<span class="containsWord">';
+        newHtml += sentence.substring(indexes[0][0], indexes[0][0]+indexes[0][1])
+        newHtml += '</span>';
+        newHtml += sentence.substring(indexes[0][0]+indexes[0][1])
+        return newHtml;
+    } else{
+        newHtml += sentence.substring(startIndex, indexes[0][0]);
+        newHtml += '<span class="containsWord">';
+        newHtml += sentence.substring(indexes[0][0], indexes[0][0]+indexes[0][1])
+        newHtml += '</span>';
+    }
+    return newHtml + sentenceReconstructor(sentence, indexes[0][0]+indexes[0][1], indexes.slice(1));
+}
+
+
 // Given a list of text sentences, returns a string with sentences separated by <br> tags.
 function getSentenceHTML(sentenceList) {
     var html = '';
     while (sentenceList.length > 0) {
         var currentSentence = sentenceList.shift();
+        var indexes = findWords(currentSentence);
         if (isQuestion(currentSentence)) {
             html += '<span class="question">';
             html += currentSentence;
-            html += '</span>'
+            html += '</span>';
+        } else if(indexes.length > 0){
+            indexes.sort(function(a,b){
+                return a[0] - b[0];
+            })
+            // console.log(indexes);
+            // console.log(sentenceReconstructor(currentSentence, 0, indexes))
+            html += sentenceReconstructor(currentSentence, 0, indexes);
         } else {
             html += currentSentence;
         }
